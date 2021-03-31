6453ae1e2edac9d41680220ef35931b33e82605b
h0dah <h.elbadrawe@gmail.com>
2021-03-31 23:29:43 +0200

    fetch data for 30 days ago from github API

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask import Flask
+import urllib.request
+import datetime
+import json
 app = Flask(__name__)
-app.config["DEBUG"] = True
-
 
 @app.route('/top_repos', methods=['GET'])
 def top_repos():
+    start_date = datetime.datetime.now() - datetime.timedelta(30)
+    start_date = start_date.strftime("%Y-%m-%d")
+    url = f"https://api.github.com/search/repositories?q=created:>{start_date}&sort=stars&order=desc"
+
+    with urllib.request.urlopen(url) as json_url:
+        data = json.loads(json_url.read())
+
+    
     return 'it works'
